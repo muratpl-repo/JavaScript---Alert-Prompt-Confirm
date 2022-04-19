8d6bf4a1cceb78a04dfe13d3dc2b1ca093c3132a
muratpala007 <muratpala007@gmail.com>
2022-04-19 22:28:28 +0300

    js file added

---
+++ myscripts.js
@@ -0,0 +1,13 @@
+var kulaniciGirisi = alert("Bu bir alerts kutusudur!");
+
+        var yasHesapla = prompt("Lutfen dogum yilinizi giriniz...");
+        document.write((2022-yasHesapla) + " yasindasiniz... ");
+
+        /*
+        var kullaniciGirisi = confirm("Bu işlemi onaylıyor musunuz?");
+        if (kullaniciGirisi){
+            document.write("Bu islemi onayladiniz...");
+        }
+        else if (!kullaniciGirisi) {
+            document.write("Bu islemi onaylaMAdiniz...");
+        }*/
